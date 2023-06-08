d26f8b45159a6aed2cf3e606cc8e71a54634d7ad
stefansimereaa <stefanosimerea16@gmail.com>
2023-06-08 17:51:42 +0200

    fixed code and added sounds ecc

--- js/script.js
+++ js/script.js
@@ -6,7 +6,10 @@ const button = document.getElementById('play');
 const selectedLevel = document.getElementById('levels');
 const scoreElement = document.getElementById('score');
 const explosionSound = document.getElementById('explosion-sound');
-
+const winSound = document.getElementById('win-sound');
+const loseSound = document.getElementById('lose-sound');
+const winImage = document.getElementById('win');
+const loseImage = document.getElementById('lose');
 
 let rows;
 let cols;
@@ -62,17 +65,48 @@ const generateBombs = () => {
 const updateScore = () => {
   score++;
   scoreElement.innerText = 'Score: ' + score;
+  checkGameEnd();
+};
+
+// Funzione per verificare la fine del gioco
+const checkGameEnd = () => {
+    if (score === totalCells - bombs.length) {
+      endGame(true);
+    }
+};
+
+// Funzione per mostrare a fine partita tutte le celle
+const revealCells = () => {
+    const cells = grid.getElementsByClassName('cell');
+    for (let i = 0; i < cells.length; i++) {
+      const cell = cells[i];
+      cell.classList.remove('hidden');
+      if (bombs.includes(parseInt(cell.innerText))) {
+        cell.classList.add('bomb');
+      } else {
+        cell.classList.add('clicked');
+      }
+    }
 };
 
 // Funzione per terminare il gioco
 const endGame = (isWin) => {
   gameEnded = true;
   scoreElement.innerText = 'Score: ' + score;
-  const message = isWin ? 'Congratulazioni hai vinto!' : 'Game Over! :(';
   const result = document.getElementById('result');
-  result.innerText = message;
-  explosionSound.play();
 
+  if (isWin) {
+    winImage.style.display = 'block';
+    winSound.play();
+    loseImage.style.display = 'none';
+  }else {
+    loseImage.style.display = 'block';
+    loseSound.play();
+    winImage.style.display = 'none';
+  }
+
+  revealCells();
+  explosionSound.play();
 };
 
 // Controllo Click del pulsante Play
@@ -109,6 +143,4 @@ button.addEventListener('click', function () {
         cell.innerText = i + 1;
         grid.appendChild(cell);
     }
-  
-
 });
